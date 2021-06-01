5bcf5dfd8bfe4a7932e7716b491a0d65ac93fbba
Rodrigo Prado de Jesus <royopa@gmail.com>
2021-06-01 16:46:49 -0300

    implementado documentação

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -18,4 +18,31 @@ class DefaultController extends AbstractController
             'controller_name' => 'DefaultController',
         ]);
     }
+
+    /**
+     * @Route("/product_backlog", name="product_backlog_index")
+     */
+    public function product_backlog(): Response
+    {
+        return $this->render('documentos/product_backlog.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/diagrama_casos_uso", name="diagrama_casos_uso_index")
+     */
+    public function diagrama_casos_uso(): Response
+    {
+        return $this->render('documentos/diagrama_casos_uso.html.twig', [
+        ]);
+    }
+    
+    /**
+     * @Route("/diagrama_classes", name="diagrama_classes_index")
+     */
+    public function diagrama_classes(): Response
+    {
+        return $this->render('documentos/diagrama_classes.html.twig', [
+        ]);
+    }    
 }
